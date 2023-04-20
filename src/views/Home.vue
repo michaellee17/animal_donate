4713fd3317294957a58fc09d29e58c130d4eba19
jayzeng <jay.zeng@baifu-tech.net>
2023-04-20 17:44:44 +0800

    feat:手機版選單

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,8 +9,8 @@ const modules = [Autoplay];
     <div>
         <swiper :slides-per-view="1" :modules="modules" :autoplay="{ delay: 3000 }">
             <swiper-slide><img src="assets/indexTop.png" alt=""></swiper-slide>
-            <swiper-slide>Slide 2</swiper-slide>
-            <swiper-slide>Slide 3</swiper-slide>
+            <swiper-slide><img src="assets/indexTop.png" alt=""></swiper-slide>
+            <swiper-slide><img src="assets/indexTop.png" alt=""></swiper-slide>
 
         </swiper>
         
